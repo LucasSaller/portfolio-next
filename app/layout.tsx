0a3ffc14f6084133cb301af9b4a77808c2d7a608
LucasSaller <sallerlucas@gmail.com>
2024-05-23 14:13:08 -0300

    init portfolio project <3

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Lucas's Portfolio",
+  description: "some things about me :D",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
