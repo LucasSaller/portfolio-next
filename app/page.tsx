dbaefdd150c89d2a81db7715bf83d612adf1fa94
LucasSaller <sallerlucas@gmail.com>
2024-05-31 16:00:42 -0300

    fix scrolling bug

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { navItems } from "@/data/index";
 import Link from "next/link";
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full block">
         <FloatingNav navItems={navItems} />
         <Hero />
